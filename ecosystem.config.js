8be7edfecbc6d68f22cef1a9b6e6e6b8c8c20656
khanz1 <assistance.xavier@gmail.com>
2023-04-16 14:21:39 +0700

    chore(production): add pm2 configuration

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'docs.khanz1.dev',
+      script: 'bash',
+      args: '-c "npm run build && npm start"',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+      },
+      instances: 'max',
+      exec_mode: 'cluster',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      log_date_format: 'YYYY-MM-DD HH:mm Z',
+    },
+  ],
+};
