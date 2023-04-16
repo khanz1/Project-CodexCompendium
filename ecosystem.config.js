3ebd8b33c2426733439f190fbabfa469b64e94a1
khanz1 <assistance.xavier@gmail.com>
2023-04-16 15:14:24 +0700

    chore(pm2): remove cluster setting

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,8 +8,6 @@ module.exports = {
         NODE_ENV: 'production',
         PORT: 3000,
       },
-      instances: 'max',
-      exec_mode: 'cluster',
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
